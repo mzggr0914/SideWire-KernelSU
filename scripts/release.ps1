3577fcbdbce99d43e2ea2a403f8ff5fe4a337cca
mzggr0914 <mzggr0914@gmail.com>
2026-09-03 01:25:30 +0900

    ci: test Windows and Linux hosts

--- scripts/release.ps1
+++ scripts/release.ps1
@@ -66,7 +66,7 @@ try {
     if ($entries | Where-Object { $_ -match '\\' }) {
         throw "Release ZIP contains Windows-style path separators"
     }
-    foreach ($required in @('module.prop', 'webroot/index.html', 'bin/sidewired', 'bin/sidewirectl', 'bin/sidewire-clipboard.jar')) {
+    foreach ($required in @('module.prop', 'action.sh', 'webroot/index.html', 'bin/sidewired', 'bin/sidewirectl', 'bin/sidewire-clipboard.jar')) {
         if ($entries -notcontains $required) {
             throw "Release ZIP is missing required entry: $required"
         }
